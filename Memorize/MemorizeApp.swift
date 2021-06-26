8e4c4e4f848983b4b307217ccabd217e6ea56d88
sidharth-ping <sidharth@pingthis.in>
2021-06-26 13:06:10 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sidharth Reji kumar on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
